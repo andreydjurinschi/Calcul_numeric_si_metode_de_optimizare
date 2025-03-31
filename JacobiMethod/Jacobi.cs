7ee168ade4fe440f00191b72a81165b1feb8b8fa
Andrei <djurinschi.andreiai@gmail.com>
2025-03-31 12:31:04 +0300

    FInished Jacobi method

--- JacobiMethod/Jacobi.cs
+++ JacobiMethod/Jacobi.cs
@@ -1,49 +1,29 @@
-﻿namespace GaussSeidel_Method_;
+﻿namespace JacobiMethod;
 
-public class GaussSeidel
+public class Jacobi
 {
-    
-    /**
-     * _array - matrix A
-     * _BMatrix - matrix B
-     * _epsilon - 0,001 (10^-6)
-     */
     private double[,] _array;
-    private double[] _BMatrix;
+    private double[] _B_array;
     private readonly double _epsilon = Math.Pow(10, -3);
-    
-    /**
-     * Constructor - array (Matrix A), array(Matrix b)
-     */
-    public GaussSeidel(double[,] array, double[] bMatrix)
+
+    public Jacobi(double[,] array,  double[]  b_array)
     {
-        _array = array;
-        _BMatrix = bMatrix;
+        this._array = array;
+        this._B_array = b_array;
     }
-    
-    /**
-     * X generation dor every iteration
-     * for every x I calculate it based on Gauss-Seidel method
-     *
-     * return type is an array
-     */
+
     private double[] GenerateX(double[] xPrev)
     {
-        int n = _BMatrix.Length;
+        int n = _B_array.Length;
         double[] xNew = new double[n];
-
-        xNew[0] = 1 / _array[0, 0] * (_BMatrix[0] - (_array[0, 1] * xPrev[1] + _array[0, 2] * xPrev[2] + _array[0, 3] * xPrev[3]));
-        xNew[1] = 1 / _array[1, 1] * (_BMatrix[1] - (_array[1, 0] * xPrev[0] + _array[1, 2] * xPrev[2] + _array[1, 3] * xPrev[3]));
-        xNew[2] = 1 / _array[2, 2] * (_BMatrix[2] - (_array[2, 0] * xPrev[0] + _array[2, 1] * xPrev[1] + _array[2, 3] * xPrev[3]));
-        xNew[3] = 1 / _array[3, 3] * (_BMatrix[3] - (_array[3, 0] * xPrev[0] + _array[3, 1] * xPrev[1] + _array[3, 2] * xPrev[2]));
+        
+        xNew[0] = (_B_array[0] - _array[0,1] * xPrev[1] - _array[0,2] * xPrev[2] - _array[0,3] * xPrev[3]) / _array[0,0];
+        xNew[1] = (_B_array[1] - _array[1,0] * xPrev[0] - _array[1,2] * xPrev[2] - _array[1,3] * xPrev[3]) / _array[1,1];
+        xNew[2] = (_B_array[2] - _array[2,0] * xPrev[0] - _array[2,1] * xPrev[1] - _array[2,3] * xPrev[3]) / _array[2,2];
+        xNew[3] = (_B_array[3] - _array[3,0] * xPrev[0] - _array[3,1] * xPrev[1] - _array[3,2] * xPrev[2]) / _array[3,3];
         return xNew;
     }
-
-    /**
-     * function than calculate convergence
-     * params: xPrev - x(n); xNext - x(n+1)
-     * return: max value - max
-     */
+    
     private double Convergence(double[] xPrev, double[] xNext)
     {
         double max = 0;
@@ -57,10 +37,7 @@ public class GaussSeidel
         }
         return max;
     }
-
-    /**
-     * Printing the matrix
-     */
+    
     private void PrintMatrix(double[,] arr)
     {
         for (int i = 0; i < arr.GetLength(0); i++)
@@ -72,10 +49,7 @@ public class GaussSeidel
             Console.WriteLine();
         }
     }
-
-    /**
-     * Printing an intermediate X values
-     */
+    
     private void PrintIntermediateX(double[] arr)
     {
         foreach (var item in arr)
@@ -85,16 +59,12 @@ public class GaussSeidel
 
         Console.WriteLine();
     }
-
     
-    /**
-     * Usage of all methods in this class
-     */
-    public double[] CreateGaussSeidel()
+    public double[] CreateJacobi()
     {
-        Console.WriteLine("Creating Gauss Seidel for: ");
+        Console.WriteLine("Creating Jacobi for: ");
         PrintMatrix(_array);
-        int n = _BMatrix.Length;
+        int n = _B_array.Length;
         double[] xPrev = new double[n];
         double[] xNext = new double[n];
         double maxDifference;
@@ -118,8 +88,6 @@ public class GaussSeidel
             Console.WriteLine("previous:");
             PrintIntermediateX(xPrev);
             Console.WriteLine("next:");
-            Console.WriteLine("Max:");
-            Console.WriteLine(maxDifference);
             PrintIntermediateX(xNext);
             step++;
             Console.WriteLine("_____________________________________________________________________________");
@@ -129,8 +97,7 @@ public class GaussSeidel
         PrintIntermediateX(xNext);
         return xNext;
     }
-
-
-
+    
+    
 
 }
---
+++ JacobiMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using JacobiMethod;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        double[,] matrix = {
+            { 4.855, 1.239, 0.272, 0.258 },
+            { 1.491, 4.954, 0.124, 0.236 },
+            { 0.456, 0.285, 4.354, 0.254 },
+            { 0.412, 0.335, 0.158, 2.874 }
+        };
+
+        double[] bMatrix = { 1.192, 0.256, 0.852, 0.862 };
+        
+        Jacobi gaussSeidel = new Jacobi(matrix, bMatrix);
+        gaussSeidel.CreateJacobi();
+    }    
+}
